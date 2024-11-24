b305800eb9f50a0acabe5fe92023da85a6d1a64c
Lee gangho <rkdgh1190@gmail.com>
2024-11-24 18:08:27 +0900

    feat : Github 링크 추가

--- src/components/chip/Profile.tsx
+++ src/components/chip/Profile.tsx
@@ -1,20 +1,46 @@
-import { FaUser, FaBirthdayCake, FaPhone, FaEnvelope } from "react-icons/fa";
+import { FaUser, FaBirthdayCake, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Profile = () => {
   const profileArr = [
     { id: "이름", value: "이강호", icon: <FaUser /> },
-    { id: "생년월일", value: "00.09.02", icon: <FaBirthdayCake /> },
-    { id: "전화번호", value: "010-5497-0058", icon: <FaPhone /> },
+    { id: "나이", value: "25세", icon: <FaBirthdayCake /> },
+    {
+      id: "깃허브",
+      value: "chickengir1",
+      icon: <FaGithub />,
+      url: "https://github.com/chickengir1",
+    },
     { id: "이메일", value: "rkdgh1198@gmail.com", icon: <FaEnvelope /> },
   ];
 
   const profile = profileArr.map((item) => {
+    if (item.id === "깃허브") {
+      // 깃허브 항목에 링크 적용
+      return (
+        <a
+          key={item.id}
+          href={item.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-4 flex items-center space-x-4 bg-[#3A3A3A] border border-[#555] rounded-lg"
+        >
+          <div className="flex items-center justify-center text-4xl text-[#ccc] transition-transform hover:text-[#00ADB5] hover:scale-105">
+            {item.icon}
+          </div>
+          <div>
+            <div className="font-bold text-lg text-white">{item.id}</div>
+            <div className="text-sm text-gray-300">{item.value}</div>
+          </div>
+        </a>
+      );
+    }
+
     return (
       <div
         key={item.id}
         className="p-4 flex items-center space-x-4 bg-[#3A3A3A] border border-[#555] rounded-lg"
       >
-        <div className="flex items-center justify-center text-4xl text-[#00ADB5]">
+        <div className="flex items-center justify-center text-4xl text-[#ccc]">
           {item.icon}
         </div>
         <div>
