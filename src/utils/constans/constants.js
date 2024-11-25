c3b13d3caac3c6db3ca998bfe8b85f0cc36b9eb1
Lee gangho <rkdgh1190@gmail.com>
2024-11-25 20:32:48 +0900

    fix : 프로젝트 순서 변경

--- src/utils/constans/constants.js
+++ src/utils/constans/constants.js
@@ -65,15 +65,6 @@ export const skills = [
 export const projects = [
   {
     id: 1,
-    title: "엘리스 스토어",
-    description: "엘리스 Iot트랙 2차 프로젝트 e-commerce 쇼핑몰 입니다.",
-    features: ["배송 수정, 상품 수정", "상품 검색 기능", "카테고리 별 정렬"],
-    url: "",
-    github: "https://github.com/chickengir1/Iot-3-e-commerce",
-    techStack: ["Styled-components", "JavaScript", "Vite"],
-  },
-  {
-    id: 2,
     title: "IoT 스마트팜",
     description: "엘리스 Iot트랙 3차 프로젝트 스마트팜 웹 애플리케이션입니다.",
     features: [
@@ -86,7 +77,25 @@ export const projects = [
     techStack: ["Vite", "JavaScript", "Redux", "MUI"],
   },
   {
+    id: 2,
+    title: "엘리스 스토어",
+    description: "엘리스 Iot트랙 2차 프로젝트 e-commerce 쇼핑몰 입니다.",
+    features: ["배송 수정, 상품 수정", "상품 검색 기능", "카테고리 별 정렬"],
+    url: "",
+    github: "https://github.com/chickengir1/Iot-3-e-commerce",
+    techStack: ["Styled-components", "JavaScript", "Vite"],
+  },
+  {
     id: 3,
+    title: "포트폴리오 프로젝트",
+    description: "프론트엔드 포트폴리오 웹 사이트입니다.",
+    features: ["Three.js를 이용한 워드 클라우드", "SVG Path Animation"],
+    url: "https://chickengir1.github.io/portfolio/",
+    github: "https://github.com/chickengir1/portfolio",
+    techStack: ["TypeScript", "Three.js", "Vite", "Tailwind CSS"],
+  },
+  {
+    id: 4,
     title: "디스코드봇 대시보드",
     description: "Friendly AI 해커톤에서 개발한 디스코드 봇 생성기입니다.",
     features: ["디스코드 봇 커스텀 UI", "대시보드로 봇 전체 관리"],
@@ -95,21 +104,12 @@ export const projects = [
     techStack: ["React", "OAuth2.0"],
   },
   {
-    id: 4,
+    id: 5,
     title: "AI 이미지 생성기",
     description: "프롬프트 기반 이미지 생성 애플리케이션입니다.",
     features: ["프롬프트 기반 이미지 생성", "생성된 이미지를 URL로 변환"],
     url: "https://aimg-15ctjutvs-lees-projects-1cb4a49b.vercel.app/",
     github: "https://github.com/chickengir1/aimg",
     techStack: ["JavaScript", "OpenAI DALL·E"],
-  },
-  {
-    id: 5,
-    title: "포트폴리오 프로젝트",
-    description: "프론트엔드 포트폴리오 웹 사이트입니다.",
-    features: ["Three.js를 이용한 워드 클라우드", "SVG Path Animation"],
-    url: "https://chickengir1.github.io/portfolio/",
-    github: "https://github.com/chickengir1/portfolio",
-    techStack: ["TypeScript", "Three.js", "Vite", "Tailwind CSS"],
   },
 ];
