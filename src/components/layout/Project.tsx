de6272470ca3da1d74b149f26d145034f4c710ed
Lee gangho <rkdgh1190@gmail.com>
2024-11-25 14:49:53 +0900

    Fix : 텍스트 색상 수정

--- src/components/layout/Project.tsx
+++ src/components/layout/Project.tsx
@@ -57,7 +57,7 @@ const Project = () => {
       <h2 className="text-3xl text-center font-bold text-gray-900 my-8 sm:text-4xl md:text-5xl">
         My Projects
       </h2>
-      <h3 className="text-base text-center font-semibold text-gray-900 my-4 sm:text-lg md:text-xl">
+      <h3 className="text-base text-center font-semibold text-blue-500 mb-4 sm:text-lg md:text-xl">
         폴더를 클릭하면 깃허브로 이동 할 수 있어요
       </h3>
       <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-8">
