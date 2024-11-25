22a7c8c4d1e71f2a4f5079e5d4f288ec6489c21a
Lee gangho <rkdgh1190@gmail.com>
2024-11-25 15:44:41 +0900

    fix : profile정보 나이 -> linkedIn으로 수정

--- src/components/chip/Profile.tsx
+++ src/components/chip/Profile.tsx
@@ -1,9 +1,14 @@
-import { FaUser, FaBirthdayCake, FaGithub, FaEnvelope } from "react-icons/fa";
+import { FaUser, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Profile = () => {
   const profileArr = [
     { id: "이름", value: "이강호", icon: <FaUser /> },
-    { id: "나이", value: "25세", icon: <FaBirthdayCake /> },
+    {
+      id: "링크드인",
+      value: "profile",
+      icon: <FaLinkedin />,
+      url: "https://www.linkedin.com/in/%EA%B0%95%ED%98%B8-%EC%9D%B4-5b38b9317/",
+    },
     {
       id: "깃허브",
       value: "chickengir1",
@@ -14,7 +19,7 @@ const Profile = () => {
   ];
 
   const profile = profileArr.map((item) => {
-    if (item.id === "깃허브") {
+    if (item.id === "깃허브" || item.id === "링크드인") {
       return (
         <a
           key={item.id}
