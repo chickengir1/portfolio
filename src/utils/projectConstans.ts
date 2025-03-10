c2816a6f2b65e8c255aa51a227f04dff5707db7c
Lee gangho <rkdgh1198@gmail.com>
2025-03-11 00:51:21 +0900

    refactor: 프로젝트 상수 파일의 프로젝트 설명 및 기능 세부 정보 업데이트
    - 각 프로젝트의 설명과 기능 목록을 더 명확하고 간결하게 수정
    - GitHub 및 URL 링크 업데이트
    - 프로젝트 기술 스택 및 특징 문구 개선

--- src/utils/projectConstans.ts
+++ src/utils/projectConstans.ts
@@ -6,11 +6,10 @@ export const PROJECTS = [
       "모두를 위한 인공지능 비서 Ai-zy 프로젝트 입니다. 테스트를 원하신다면 아래 계정을 프로젝트 생성하실때 추가해주세요",
     test: "eetd100@naver.com",
     features: [
-      "Auth0를 기반 로그인",
-      "OpenAI Api를 활용한 챗 봇 어시스턴트",
-      "OpenAI Api를 활용한 문서 요약 및 일정 생성",
+      "OpenAI Api를 활용한 챗 봇 어시스턴트 및 문서 요약,일정 생성 기능",
+      "Auth0 기반 로그인 및 소셜 로그인",
       "React-Query 기반 서버 상태 관리",
-      "Zustand 기반 전역 상태 관리",
+      "Zustand를 활용한 전역 상태 관리",
       "Storybook과 Chromatic을 통한 스토리북 테스트 및 문서 배포",
       "Gitlab CI/CD를 통한 배포 자동화",
     ],
@@ -23,17 +22,16 @@ export const PROJECTS = [
     id: "vite-plugin-pages-router",
     title: "vite-plugin-pages-router",
     description:
-      "Vite 환경에서도 next.js의 pages-router처럼 사용할 수 있는 Vite 플러그인 입니다.",
+      "Vite 환경에서도 next.js의 pages-router와 같은 파일 기반 라우팅 시스템을 지원하는 Vite 플러그인 입니다.",
     features: [
-      "Suspense를 활용한 지연 로딩 최적화",
-      "슬러그 기반 동적 라우팅 지원",
-      "Error Boundary를 활용한 404 페이지 지원",
-      "next.js pages-router와 같은 파일 기반 라우팅 시스템",
+      "Next.js pages-router와 같은 파일 기반 라우팅 시스템",
       "vite glob Api 기반 파일 자동 인식",
+      "React Suspense를 활용한 지연 로딩 최적화",
+      "React Error Boundary를 활용한 404 페이지 지원",
       "Vitest를 활용한 페이지 라우팅 인식 테스트 코드 작성",
     ],
     url: "https://www.npmjs.com/package/vite-plugin-pages-router",
-    github: "https://github.com/chickengir1/vite-plugin-pages-router",
+    github: "https://github.com/chickengir1/example-vite-plugin-pages-router",
     techStack: ["Vite", "Vitest", "Rollup"],
   },
   {
@@ -59,12 +57,12 @@ export const PROJECTS = [
     description:
       "센서 정보를 바탕으로 IoT 기기들을 제어할 수 있는 웹 애플리케이션 및 디바이스를 구현한 프로젝트입니다. 스마트폰을 통해 화분의 상태를 실시간으로 모니터링하고, 원격으로 화분을 컨트롤합니다.",
     features: [
+      "d3.js를 활용한 커스텀 차트 시각화",
+      "polling 기반 주기적 데이터 업데이트",
+      "MUI 기반 컴포넌트 디자인",
+      "Redux를 활용한 전역 상태 관리",
       "geolocation API를 활용한 위치 기반 날씨 정보 가져오기",
       "openAPI 기반 뉴스레터 구독",
-      "d3.js를 활용한 커스텀 차트 시각화",
-      "MUI 기반 컴포넌트 디자인",
-      "polling 기반 주기적 데이터 업데이트",
-      "Redux를 활용한 전역 상태 관리",
     ],
     url: "",
     github:
@@ -79,10 +77,10 @@ export const PROJECTS = [
     features: [
       "React-hook-form 기반 폼 유효성 검사",
       "Styled-components 기반 스타일링",
-      "Dashboard 기반 상품 관리",
     ],
     url: "",
-    github: "https://github.com/chickengir1/e-commerce",
+    github:
+      "https://cooked-cord-a64.notion.site/Elice-Store-E-commerce-1b2b9ca10d0d8022825ceebb5078e4b1",
     techStack: ["Styled-components", "JavaScript", "Vite"],
   },
   {
@@ -94,7 +92,7 @@ export const PROJECTS = [
       "HTML, CSS, JavaScript를 활용한 프로젝트",
       "기존 보일러 플레이트를 직접 커스텀",
     ],
-    url: "https://land-bk4ytdel7-lees-projects-1cb4a49b.vercel.app/?vercelToolbarCode=keN0TNo8hSUYmhg",
+    url: "https://land-rose.vercel.app/",
     github: "https://github.com/chickengir1/land",
     techStack: ["HTML", "CSS", "JavaScript"],
   },
@@ -102,10 +100,10 @@ export const PROJECTS = [
     id: "디스코드 봇",
     title: "사용자와 친구가 되어주는 디스코드 봇",
     description:
-      "이 프로젝트는 딱딱한 관리자 역할을 벗어나, 감성 있고 재치 있는 친구 같은 Discord Bot을 제공합니다. 이를 통해 Discord 서버의 특색에 맞춘 자아와 기능을 가진 봇과 이를 설정할 수 있는 대시보드 웹 인터페이스를 구현합니다.",
+      "Discord 서버의 특색에 맞춘 자아와 기능을 가진 봇과 이를 설정할 수 있는 대시보드 웹 인터페이스를 구현한 프로젝트 입니다.",
     features: [
       "OAuth2.0를 활용한 디스코드 로그인",
-      "허깅페이스를 활용한 모델 설정",
+      "FriendliAI의 Serverless Endpoint를 이용한 정보 제공",
     ],
     url: "https://chickengir1.github.io/LLMfront/",
     github: "https://github.com/chickengir1/LLMfront",
@@ -115,12 +113,12 @@ export const PROJECTS = [
     id: "AI 이미지 생성기",
     title: "OpenAI API를 활용한 AI 이미지 생성기",
     description:
-      "Aimg는 사용자가 입력한 프롬프트를 바탕으로 AI 기반 이미지를 생성하는 웹 애플리케이션입니다. OpenAI API를 활용하여 사용자에게 독창적이고 맞춤화된 AI 생성 이미지를 제공합니다.",
+      "OpenAI의 DALL·E 2 API를 활용하여 사용자가 입력한 프롬프트를 바탕으로 AI 기반 이미지를 생성하는 웹 애플리케이션입니다.",
     features: [
       "DALL·E 2 API를 활용한 이미지 생성",
       "프롬프트 엔지니어링을 통한 이미지 생성",
     ],
-    url: "https://aimg-15ctjutvs-lees-projects-1cb4a49b.vercel.app/",
+    url: "https://aimg-three.vercel.app/",
     github: "https://github.com/chickengir1/aimg",
     techStack: ["JavaScript", "OpenAI DALL·E"],
   },
